f6513bce21ebd70f96deb7d493616c3f432e78e8
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-02-10 23:19:53 -0300

    How to share data

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      base.OnNavigatedTo(e);
+
+      DataTransferManager dtm = DataTransferManager.GetForCurrentView();
+      dtm.DataRequested += Dtm_DataRequested;
+    }
+
+    private void Dtm_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+    {
+      DataRequest request = args.Request;
+
+      request.Data.Properties.Title = "Burger Truck";
+      request.Data.Properties.Description = "Info about the Burger Truck";
+      request.Data.SetText("Hello, World!");
+
+      request.Data.SetDataProvider(StandardDataFormats.Html, OnDeferredDataRequestedHandler);
+
+      request.Data.SetData("myappuniqueid", "Some custom data");
+    }
+
+    async void OnDeferredDataRequestedHandler(DataProviderRequest request)
+    {
+      DataProviderDeferral deferral = request.GetDeferral();
+      await Task.Delay(3000);
+
+      request.SetData("My very long text...");
+      deferral.Complete();
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+      base.OnNavigatedFrom(e);
+      DataTransferManager dtm = DataTransferManager.GetForCurrentView();
+      dtm.DataRequested -= Dtm_DataRequested;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      DataTransferManager.ShowShareUI();
+    }
+  }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+      doneBtn.IsEnabled = false;
+    }
+
+    ShareOperation _Operation;
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      base.OnNavigatedTo(e);
+
+      _Operation = e.Parameter as ShareOperation;
+
+      Task.Factory.StartNew(async () =>
+      {
+        string text = null, html = null;
+        if (_Operation.Data.Contains(StandardDataFormats.Text))
+        {
+          text = await _Operation.Data.GetTextAsync();
+        }
+        if (_Operation.Data.Contains(StandardDataFormats.Html))
+        {
+          html = await _Operation.Data.GetHtmlFormatAsync();
+        }
+
+        string custom = null;
+        if (_Operation.Data.Contains("myappuniqueid"))
+        {
+          custom = (string)await _Operation.Data.GetDataAsync("myappuniqueid");
+        }
+
+        _Operation.ReportDataRetrieved();
+
+        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+        {
+          doneBtn.IsEnabled = true;
+          this.Text1.Text = text ?? "";
+          this.Text2.Text = html ?? "";
+          this.Text3.Text = custom ?? "";
+        });
+      });
+    }
+
+    private void doneBtn_Click(object sender, RoutedEventArgs e)
+    {
+      _Operation?.ReportCompleted();
+    }
+  }
+}
